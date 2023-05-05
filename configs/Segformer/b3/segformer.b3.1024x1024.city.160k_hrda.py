37f0507b7e6f2bb81f91fe06474d3a62ab1ddd2e
Diandian Guo <guodiandian1998@163.com>
2023-05-05 16:15:53 +0200

    add daformer_conv_mitb3, debug

--- configs/Segformer/b3/segformer.b3.1024x1024.city.160k_hrda.py
+++ configs/Segformer/b3/segformer.b3.1024x1024.city.160k_hrda.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../../_base_/models/daformer_conv1_mitb5.py',
+    '../../_base_/models/daformer_conv1_mitb3.py',
     '../../_base_/datasets/cityscapes_1024x1024_repeat.py',
     '../../_base_/default_runtime.py',
     '../../_base_/schedules/schedule_160k_adamw.py'
@@ -10,14 +10,15 @@ norm_cfg = dict(type='SyncBN', requires_grad=True)
 find_unused_parameters = True
 model = dict(
     type='HRDAEncoderDecoder',
-    decode_head=dict(fusion_cfg=dict(
+    decode_head=dict(decoder_params=dict(
+            fusion_cfg=dict(
                 _delete_=True,
                 type='aspp',
                 sep=True,
                 dilations=(1, 6, 12, 18),
                 pool=False,
                 act_cfg=dict(type='ReLU'),
-                norm_cfg=norm_cfg),
+                norm_cfg=norm_cfg)),
         type='HRDAHead',
         # Use the DAFormer decoder for each scale.
         single_scale_head='DAFormerHead',
