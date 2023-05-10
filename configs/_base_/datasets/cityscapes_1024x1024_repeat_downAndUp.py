6d8ed527c5fd69b4b6a6fa6fe82fe5b9379f67c2
tonglu <tonglu@ethz.student.ch>
2023-05-10 14:07:21 +0200

    test downup

--- configs/_base_/datasets/cityscapes_1024x1024_repeat_downAndUp.py
+++ configs/_base_/datasets/cityscapes_1024x1024_repeat_downAndUp.py
@@ -7,7 +7,8 @@ crop_size = (1024, 1024)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
-    dict(type='Resize', img_scale=(1024, 512)),
+    dict(type='Resize', img_scale=(1024, 512), keep_ratio=True),
+    dict(type='Resize', img_scale=(2048, 1024), keep_ratio=True),
     dict(type='Resize', img_scale=(2048, 1024), ratio_range=(0.5, 2.0)),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
