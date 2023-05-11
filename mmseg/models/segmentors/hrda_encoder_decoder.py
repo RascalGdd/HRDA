e69aa3d65a7622ac2a60b6340620aef9d4c7a774
tonglu <tonglu@ethz.student.ch>
2023-05-11 15:11:37 +0200

    depth map loading and testing

--- mmseg/models/segmentors/hrda_encoder_decoder.py
+++ mmseg/models/segmentors/hrda_encoder_decoder.py
@@ -36,20 +36,22 @@ def get_crop_bbox_vanish_point(depth_map, crop_size, divisible=1):
         return (0, img_h, 0, img_w)
 
     # max_val: (B,1,1,1)
-    max_val = torch.amax(depth_map, dim=(2, 3)).unsqueeze(-1).unsqueeze(-1)
+    # max_val = torch.amax(depth_map, dim=(2, 3)).unsqueeze(-1).unsqueeze(-1)
+    max_val = torch.max(depth_map).item()
     # max_val_ids: (L,4), e.g., [[0,0,0,0], [0,0,0,1],..., [3,0,16,501]] (list of max_value_points)
     max_val_ids = (depth_map == max_val).nonzero(as_tuple = False)
 
     central_id = max_val_ids.float().mean(dim=0).long()[2:]  # (h,w)
-    if central_id[0] % 4 != 0:
-        central_id[0] = central_id[0] - central_id[0] % 4
-    if central_id[1] % 4 != 0:
-        central_id[1] = central_id[1] - central_id[1] % 4
+    if divisible:
+        if central_id[0] % 4 != 0:
+            central_id[0] = central_id[0] - central_id[0] % 4
+        if central_id[1] % 4 != 0:
+            central_id[1] = central_id[1] - central_id[1] % 4
 
-    crop_y1 = (central_id[0] - crop_size[-2] / 2).long()
-    crop_y2 = (central_id[0] + crop_size[-2] / 2).long()
-    crop_x1 = (central_id[1] - crop_size[-1] / 2).long()
-    crop_x2 = (central_id[1] + crop_size[-1] / 2).long()
+    crop_y1 = (central_id[0] - crop_size[0] / 2).long()
+    crop_y2 = (central_id[0] + crop_size[0] / 2).long()
+    crop_x1 = (central_id[1] - crop_size[1] / 2).long()
+    crop_x2 = (central_id[1] + crop_size[1] / 2).long()
 
     if crop_y1 < 0:
         crop_y2 += - crop_y1
