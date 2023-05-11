bd4a1f44be8c8817810e97c22d2b2887c040deb3
tonglu <tonglu@ethz.student.ch>
2023-05-11 16:06:05 +0200

    depth map debug

--- mmseg/models/segmentors/hrda_encoder_decoder.py
+++ mmseg/models/segmentors/hrda_encoder_decoder.py
@@ -235,7 +235,7 @@ class HRDAEncoderDecoder(EncoderDecoder):
                     save_image(scaled_img[0,:3,:,:], 'debug/{}_cropped_image.png'.format(self.debug_count))
                     crop_mask = img[0,3:,:,:] * 0
                     crop_y1, crop_y2, crop_x1, crop_x2 = roi_crop_box
-                    crop_mask[:, :, crop_y1:crop_y2, crop_x1:crop_x2] = 1
+                    crop_mask[:, crop_y1:crop_y2, crop_x1:crop_x2] = 1
                     save_image(crop_mask, 'debug/{}_crop_mask.png'.format(self.debug_count))
                     for feat in mres_feats[-1]:
                         print("mres feat shapes:", feat.shape)
