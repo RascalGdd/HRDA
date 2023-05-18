71b427995c826bcd928b0ac1dedfb6bf025c1072
Diandian Guo <guodiandian1998@163.com>
2023-05-18 11:38:14 +0200

    rewrite con2d, add init registry

--- mmseg/models/decode_heads/trans_scale_head.py
+++ mmseg/models/decode_heads/trans_scale_head.py
@@ -16,7 +16,46 @@ from .decode_head import BaseDecodeHead
 from .sep_aspp_head import DepthwiseSeparableASPPModule
 import math
 import fvcore.nn.weight_init as weight_init
-from detectron2.layers import Conv2d
+
+class Conv2d(torch.nn.Conv2d):
+    """
+    A wrapper around :class:`torch.nn.Conv2d` to support empty inputs and more features.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Extra keyword arguments supported in addition to those in `torch.nn.Conv2d`:
+
+        Args:
+            norm (nn.Module, optional): a normalization layer
+            activation (callable(Tensor) -> Tensor): a callable activation function
+
+        It assumes that norm layer is used before activation.
+        """
+        norm = kwargs.pop("norm", None)
+        activation = kwargs.pop("activation", None)
+        super().__init__(*args, **kwargs)
+
+        self.norm = norm
+        self.activation = activation
+
+    def forward(self, x):
+        # torchscript does not support SyncBatchNorm yet
+        # https://github.com/pytorch/pytorch/issues/40507
+        # and we skip these codes in torchscript since:
+        # 1. currently we only support torchscript in evaluation mode
+        # 2. features needed by exporting module to torchscript are added in PyTorch 1.6 or
+        # later version, `Conv2d` in these PyTorch versions has already supported empty inputs.
+
+        x = F.conv2d(
+            x, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups
+        )
+        if self.norm is not None:
+            x = self.norm(x)
+        if self.activation is not None:
+            x = self.activation(x)
+        return x
+
 
 class PositionEmbeddingSine(nn.Module):
     """
