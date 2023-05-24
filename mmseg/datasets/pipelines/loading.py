e98e995cbeca8f7aa86bcaab4f53451804a2f06a
tonglu <tonglu@ethz.student.ch>
2023-05-24 18:01:16 +0200

    posemb

--- mmseg/datasets/pipelines/loading.py
+++ mmseg/datasets/pipelines/loading.py
@@ -57,10 +57,10 @@ def get_global_pos_emb(image_size, emb_dim):
         pos_x = x_embed[:, :, None] / dim_t
         pos_y = y_embed[:, :, None] / dim_t
         pos_x = torch.stack(
-            (pos_x[:, :, :, 0::2].sin(), pos_x[:, :, :, 1::2].cos()), dim=3
+            (pos_x[:, :, 0::2].sin(), pos_x[:, :, 1::2].cos()), dim=3
         ).flatten(2)
         pos_y = torch.stack(
-            (pos_y[:, :, :, 0::2].sin(), pos_y[:, :, :, 1::2].cos()), dim=3
+            (pos_y[:, :, 0::2].sin(), pos_y[:, :, 1::2].cos()), dim=3
         ).flatten(2)
 
         get_global_pos_emb.pos = torch.cat((pos_y, pos_x), dim=2).permute(2, 0, 1) #(D, H, W)
