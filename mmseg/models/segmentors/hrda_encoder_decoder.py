35c561a8c141551ee3e961d6b7b3152f860eb6d8
tonglu <tonglu@ethz.student.ch>
2023-05-24 16:31:11 +0200

    posemb

--- mmseg/models/segmentors/hrda_encoder_decoder.py
+++ mmseg/models/segmentors/hrda_encoder_decoder.py
@@ -76,6 +76,7 @@ class GlobalPosEmbedding(nn.Module):
             id_map: Tensor, shape ``[B, 1, H, W]``
         """
         batch_size = id_map.shape[0]
+        input_H, input_W = id_map.shape
         pe_all = torch.zeros_like(id_map).repeat(1,self.emb_dim,1,1)
         id_map = id_map.to(int)
         min_ids = []
@@ -86,8 +87,8 @@ class GlobalPosEmbedding(nn.Module):
             h_min, w_min = min_1D_id % self.H, min_1D_id % self.W
             max_1D_id = torch.max(id_map[b])
             h_max, w_max = max_1D_id % self.H, max_1D_id % self.W
-            pe_all[b,:self.half_emb_dim,:,:] += self.pe_H.unsqueeze(-1).repeat(1,1,self.W)
-            pe_all[b,self.half_emb_dim:,:,:] += self.pe_W.unsqueeze(-2).repeat(1,self.H,1)
+            pe_all[b,:self.half_emb_dim,:,:] += self.pe_H.unsqueeze(-1).repeat(1,1,input_W)
+            pe_all[b,self.half_emb_dim:,:,:] += self.pe_W.unsqueeze(-2).repeat(1,input_H,1)
 
         return pe_all
 
