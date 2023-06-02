108ef76bb7b56751b02be0650013283099658ddf
tonglu <tonglu@ethz.student.ch>
2023-06-02 17:51:50 +0200

    depthmap data loading

--- mmseg/datasets/__init__.py
+++ mmseg/datasets/__init__.py
@@ -4,7 +4,9 @@
 from .acdc import ACDCDataset
 from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
 from .cityscapes import CityscapesDataset
+from .cityscapes_depth import CityscapesDatasetDepth
 from .custom import CustomDataset
+from .custom_depth import CustomDatasetDepth
 from .dark_zurich import DarkZurichDataset
 from .dataset_wrappers import ConcatDataset, RepeatDataset
 from .gta import GTADataset
@@ -13,6 +15,7 @@ from .uda_dataset import UDADataset
 
 __all__ = [
     'CustomDataset',
+    'CustomDatasetDepth',
     'build_dataloader',
     'ConcatDataset',
     'RepeatDataset',
@@ -20,6 +23,7 @@ __all__ = [
     'build_dataset',
     'PIPELINES',
     'CityscapesDataset',
+    'CityscapesDatasetDepth'
     'GTADataset',
     'SynthiaDataset',
     'UDADataset',
