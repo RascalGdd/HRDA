4b6e17f3367ea721162829379738d0b2495b755c
tonglu <tonglu@ethz.student.ch>
2023-07-20 15:46:12 +0200

    acdc video

--- configs/_base_/datasets/acdc_512x512_repeat_clips.py
+++ configs/_base_/datasets/acdc_512x512_repeat_clips.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'CityscapesDataset_clips'
-data_root = 'data/acdc_cityscapes_video/'
+data_root = 'data/acdc_video_cityscapes/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 crop_size = (512, 512)
