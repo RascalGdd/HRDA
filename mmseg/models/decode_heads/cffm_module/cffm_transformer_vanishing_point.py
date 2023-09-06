19ea054a1bc07a5922f5b6e68e62cae12f189b67
tonglu <tonglu@ethz.student.ch>
2023-09-06 21:55:03 +0200

    debug vp attn

--- mmseg/models/decode_heads/cffm_module/cffm_transformer_vanishing_point.py
+++ mmseg/models/decode_heads/cffm_module/cffm_transformer_vanishing_point.py
@@ -896,7 +896,7 @@ class WindowAttention3d3(nn.Module):
             plt.imshow(x_show_before)
             plt.savefig(f"debug/x_show_before_vp_attn_orig_{i}.png")
 
-            assert 0
+        assert 0
 
 
         central_window_id_roi = get_central_window_id_roi(
