7a80fe1f2accc7e2242085878129208e638b9a92
tonglu <tonglu@ethz.student.ch>
2023-10-30 16:50:13 +0100

    sdfm

--- mmseg/models/decode_heads/__init__.py
+++ mmseg/models/decode_heads/__init__.py
@@ -14,9 +14,7 @@ from .sep_aspp_head import DepthwiseSeparableASPPHead
 from .uper_head import UPerHead
 
 from .cffm_head import CFFMHead_clips_resize1_8
-from .cffm_head_vpattn import CFFMHead_clips_resize1_8_vpattn
 from .cffm_head_vpmove import CFFMHead_clips_resize1_8_vpmove
-from .cffm_head_vpfuse import CFFMHead_clips_resize1_8_vpfuse
 from .trans_scale_video_head import TransHeadVideo
 from .daformer_head_video import DAFormerSerialHead
 from .daformer_head_focal import DAFormerHeadFocal
